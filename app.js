534374f3339bc694c463c4d77781f33dd8404ba0
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-08 15:36:25 -0700

    question structure in JS

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var questionOne = 'Have I ever jumped from a roof?';
+var roof = prompt(questionOne);
+
+if (roof === 'yes') {
+    //congratulate
+  } else {
+    //shame
+  }
+alert('');
