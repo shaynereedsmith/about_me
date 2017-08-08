4f1ed4373c9e7d1b5da4d9f75ec303bc1c17291f
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-08 16:04:54 -0700

    Added more questions to the JS file

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 
 var questionOne = 'Have I ever jumped from a roof?';
 var roof = prompt(questionOne);
+if (roof.toLowerCase() === 'yes') {
+  alert('Correct! I have.');
+} else {
+  alert('Sorry, wrong -- but I was actually insane as a child.');
+}
+console.log('Q 1: ' + questionOne + ' | A: ' + roof);
+/*alert('On to QUESTION 2!');
 
-if (roof === 'yes') {
-    //congratulate
-  } else {
-    //shame
-  }
-alert('');
+var questionTwo = 'Have I been arrested before?';
+var arrested = prompt(questionOne);
+if (arrested.toLowerCase() === 'yes') {
+  alert('Nope! I\'m a good boy.');
+} else {
+  alert('That\'s right! Stay out of jail, kids');
+}
+
+alert('On to QUESTION 3!!');
+*/
