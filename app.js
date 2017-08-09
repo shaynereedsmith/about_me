516a4c39d26192c0b3876c0929203055097dab10
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-08 17:02:36 -0700

    added even MORE questions and made the code more clean and fancy.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 'use strict';
 
 alert('Ok, one and all -- time to answer some questions about ME! (fun, right?.... RIGHT???)');
-
-alert('First thing\'s first: Question 1!');
+alert('Now on to Question 1!');
 
 var questionOne = 'Have I ever jumped from a roof?';
-var roof = prompt(questionOne);
-if (roof.toLowerCase() === 'yes' || 'y') {
+var roof = prompt(questionOne).toLowerCase();
+if (roof === 'yes' || roof === 'y') {
   alert('Correct! I have.');
-} else {
+} else if (roof === 'no' || roof === 'n') {
   alert('Sorry, wrong -- but I was actually insane as a child. I actually have jumped from a roof');
+} else {
+  alert('You are very naughty -- play by the ruuuuuules!!!');
 }
 
 console.log('Q 1: ' + questionOne + ' | A: ' + roof);
@@ -17,11 +18,13 @@ console.log('Q 1: ' + questionOne + ' | A: ' + roof);
 alert('Ok -- on to QUESTION 2!');
 
 var questionTwo = 'Have I ever been arrested before?';
-var arrested = prompt(questionTwo);
-if (arrested.toLowerCase() === 'yes' || 'y') {
+var arrested = prompt(questionTwo).toLowerCase();
+if (arrested === 'yes' || arreste === 'y') {
   alert('Nope! I\'m a good boy.');
-} else {
+} else if (arrested === 'no' || arrested === 'n'){
   alert('That\'s right! Stay out of jail, kids');
+} else {
+  alert('You are very naughty -- play by the ruuuuuules!!!');
 }
 
 console.log('Q 2: ' + questionTwo + ' | A: ' + arrested);
@@ -29,11 +32,13 @@ console.log('Q 2: ' + questionTwo + ' | A: ' + arrested);
 alert('Now on to QUESTION 3!!');
 
 var questionThree = 'Have I ever fainted at work?';
-var faint = prompt(questionThree);
-if (faint.toLowerCase() === 'yes' || 'y') {
+var faint = prompt(questionThree).toLowerCase();
+if (faint === 'yes' || faint === 'y') {
   alert('That\'s right! It SUCKED! I was in the break room, awake and fine one moment, then the next thing I remember was staring up from the ground at 5 worried coworkers :D');
-} else {
+} else if (faint === 'no' || faint === 'n') {
   alert('No! I actually HAVE fainted. And it\s no fun, believe you me!');
+} else {
+  alert('You are very naughty -- play by the ruuuuuules!!!');
 }
 
 console.log('Q 3: ' + questionThree + ' | A: ' + faint);
@@ -41,9 +46,11 @@ console.log('Q 3: ' + questionThree + ' | A: ' + faint);
 alert('Alright! Here comes QUESTION NUMERO QUATRO (that means number 4)!!!');
 
 var questionFour = 'Have I ever been outside the continental US?';
-var outsideTheUS = prompt(questionFour);
-if (outsideTheUS.toLowerCase() === 'yes' || 'y') {
+var outsideTheUS = prompt(questionFour).toLowerCase();
+if (outsideTheUS === 'yes' || outsideTheUS === 'y') {
   alert('Unfortunately, I have lived most of my life right here in the Pacific Northwest. But I would really like to travel around a bit some day.');
-} else {
+} else if (outsideTheUS === 'no' || outsideTheUS === 'n'){
   alert('Sadly, that is correct. :(');
+} else {
+  alert('You are very naughty -- play by the ruuuuuules!!!');
 }
