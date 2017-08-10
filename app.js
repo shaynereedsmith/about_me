2654c43e27be7d404262a35039e9577efbd2aa97
kavdihodgson <kavdyjh@gmail.com>
2017-08-10 16:23:04 -0700

    more changes

--- app.js
+++ app.js
@@ -40,63 +40,71 @@ console.log('Q 2: ' + questionTwo + ' | A: ' + arrested);
 
 alert('Now on to QUESTION 3!!');
 
-var questionThree = 'Have I ever fainted at work?';
-var faint = prompt(questionThree).toLowerCase();
-if (faint === 'yes' || faint === 'y') {
-  alert('That\'s right! It SUCKED! I was in the break room, awake and fine one moment, then the next thing I remember was staring up from the ground at 5 worried coworkers :D');
-  total++;
-} else if (faint === 'no' || faint === 'n') {
-  alert('Wrong! I actually HAVE fainted. And it\s no fun, believe you me!');
-} else {
-  alert('You are very naughty -- play by the ruuuuuules!!!');
+function qThree() {
+  var questionThree = 'Have I ever fainted at work?';
+  var faint = prompt(questionThree).toLowerCase();
+  if (faint === 'yes' || faint === 'y') {
+    alert('That\'s right! It SUCKED! I was in the break room, awake and fine one moment, then the next thing I remember was staring up from the ground at 5 worried coworkers :D');
+    total++;
+  } else if (faint === 'no' || faint === 'n') {
+    alert('Wrong! I actually HAVE fainted. And it\s no fun, believe you me!');
+  } else {
+    alert('You are very naughty -- play by the ruuuuuules!!!');
+  }
 }
 
 console.log('Q 3: ' + questionThree + ' | A: ' + faint);
 
 alert('Alright! Here comes QUESTION NUMERO QUATRO (that means number 4)!!!');
 
-var questionFour = 'Have I ever been outside the continental US?';
-var outsideTheUS = prompt(questionFour).toLowerCase();
-if (outsideTheUS === 'yes' || outsideTheUS === 'y') {
-  alert('Unfortunately, no, I have lived most of my life right here in the Pacific Northwest. But I would really like to travel around a bit some day.');
-} else if (outsideTheUS === 'no' || outsideTheUS === 'n'){
-  alert('Sadly, that is correct. :(');
-  total++;
-} else {
-  alert('You are very naughty -- play by the ruuuuuules!!!');
+function qFour() {
+  var questionFour = 'Have I ever been outside the continental US?';
+  var outsideTheUS = prompt(questionFour).toLowerCase();
+  if (outsideTheUS === 'yes' || outsideTheUS === 'y') {
+    alert('Unfortunately, no, I have lived most of my life right here in the Pacific Northwest. But I would really like to travel around a bit some day.');
+  } else if (outsideTheUS === 'no' || outsideTheUS === 'n'){
+    alert('Sadly, that is correct. :(');
+    total++;
+  } else {
+    alert('You are very naughty -- play by the ruuuuuules!!!');
+  }
 }
 
 console.log('Q 4: ' + questionFour + ' | A: ' + outsideTheUS);
 
 alert('QUESTION NUMBER 5!!!!!!!1!11!!1');
 
-var questionFive = 'Have I ever read 2 books in one day?';
-var books = prompt(questionFive).toLowerCase();
-if (books === 'yes' || books === 'y') {
-  alert('I HAVE! Once upon a time, I used to read those book-things.');
-  total++;
-} else if (books === 'no' || books === 'n'){
-  alert('HAH! you underestimate me, young padawan -- I actually have read two books in one day :D');
-} else {
-  alert('You are very naughty -- play by the ruuuuuules!!!');
+function qFive() {
+  var questionFive = 'Have I ever read 2 books in one day?';
+  var books = prompt(questionFive).toLowerCase();
+  if (books === 'yes' || books === 'y') {
+    alert('I HAVE! Once upon a time, I used to read those book-things.');
+    total++;
+  } else if (books === 'no' || books === 'n'){
+    alert('HAH! you underestimate me, young padawan -- I actually have read two books in one day :D');
+  } else {
+    alert('You are very naughty -- play by the ruuuuuules!!!');
+  }
 }
 
 console.log('Q 5: ' + questionFive + ' | A: ' + books);
 
 alert('How about answering Question 6, ' + name + '?');
 
-for (var i = 0; i < 4; i++) {
-  var questionSix = 'What is my lucky number? Guess between 1 and 10.';
-  var answer = prompt(questionSix);
-  if (answer == 7) {
-    alert('Woah! You got it!');
-    var answerSix = true;
-    total++;
-    break;
-  } else if (answer < 7) {
-    alert('Sorry, that\'s too low :D');
-  } else if (answer > 7) {
-    alert('A bit too high!');
+function qSix() {
+  for (var i = 0; i < 4; i++) {
+    var questionSix = 'What is my lucky number? Guess between 1 and 10.';
+    var answer = prompt(questionSix);
+    if (answer == 7) {
+      alert('Woah! You got it!');
+      var answerSix = true;
+      total++;
+      break;
+    } else if (answer < 7) {
+      alert('Sorry, that\'s too low :D');
+    } else if (answer > 7) {
+      alert('A bit too high!');
+    }
   }
 }
 
@@ -110,19 +118,21 @@ if (answerSix) {
 
 alert(name + '! You\'ve gotten this far! :D Might as well try to answer QUESTION 7!!');
 
-for (var i = 0; i < 6; i++) {
-  var questionSeven = 'Guess my favorite food.';
-  var answer = prompt(questionSeven);
-  var food;
-  food = ['spagetti', 'pizza', 'coffee', 'noodles', 'fruit', 'veggies'];
+function qSeven() {
+  for (var i = 0; i < 6; i++) {
+    var questionSeven = 'Guess my favorite food.';
+    var answer = prompt(questionSeven);
+    var food;
+    food = ['spagetti', 'pizza', 'coffee', 'noodles', 'fruit', 'veggies'];
 
-  if (food.includes(answer.toLowerCase())) {
-    alert('WOW! That\'s right! Good job.');
-    var answerSeven = true;
-    total++;
-    break;
-  } else {
-    alert('Sorry, that\'s not one of my favorites :(');
+    if (food.includes(answer.toLowerCase())) {
+      alert('WOW! That\'s right! Good job.');
+      var answerSeven = true;
+      total++;
+      break;
+    } else {
+      alert('Sorry, that\'s not one of my favorites :(');
+    }
   }
 }
 
