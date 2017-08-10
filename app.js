d163fae510fe37d7be70ae7893746b0a366e89ef
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-09 17:26:46 -0700

    Finished the JS portion of the lab for today.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 'use strict';
-/*
-alert('Ok, one and all -- time to answer some questions about ME! (fun, right?.... RIGHT???)');
+var total = 0;
+
+alert('Hey there. I hope you are well today :D');
+var name = prompt('Please give you your name.');
+alert('Ok, -- time to answer some questions about ME!');
 alert('Now on to Question 1!');
 
 var questionOne = 'Have I ever jumped from a roof?';
 var roof = prompt(questionOne).toLowerCase();
 if (roof === 'yes' || roof === 'y') {
   alert('Correct! I have.');
+  total++;
 } else if (roof === 'no' || roof === 'n') {
   alert('Sorry, wrong -- but I was actually insane as a child. I actually have jumped from a roof');
 } else {
@@ -23,6 +27,7 @@ if (arrested === 'yes' || arrested === 'y') {
   alert('Nope! I\'m a good boy.');
 } else if (arrested === 'no' || arrested === 'n'){
   alert('That\'s right! Stay out of jail, kids');
+  total++;
 } else {
   alert('You are very naughty -- play by the ruuuuuules!!!');
 }
@@ -35,6 +40,7 @@ var questionThree = 'Have I ever fainted at work?';
 var faint = prompt(questionThree).toLowerCase();
 if (faint === 'yes' || faint === 'y') {
   alert('That\'s right! It SUCKED! I was in the break room, awake and fine one moment, then the next thing I remember was staring up from the ground at 5 worried coworkers :D');
+  total++;
 } else if (faint === 'no' || faint === 'n') {
   alert('Wrong! I actually HAVE fainted. And it\s no fun, believe you me!');
 } else {
@@ -51,18 +57,20 @@ if (outsideTheUS === 'yes' || outsideTheUS === 'y') {
   alert('Unfortunately, no, I have lived most of my life right here in the Pacific Northwest. But I would really like to travel around a bit some day.');
 } else if (outsideTheUS === 'no' || outsideTheUS === 'n'){
   alert('Sadly, that is correct. :(');
+  total++;
 } else {
   alert('You are very naughty -- play by the ruuuuuules!!!');
 }
 
 console.log('Q 4: ' + questionFour + ' | A: ' + outsideTheUS);
 
-alert('Last question, guys! And it\'s a good one -- NUMBER 5!!!!!!!1!11!!1');
+alert('QUESTION NUMBER 5!!!!!!!1!11!!1');
 
 var questionFive = 'Have I ever read 2 books in one day?';
 var books = prompt(questionFive).toLowerCase();
 if (books === 'yes' || books === 'y') {
   alert('I HAVE! Once upon a time, I used to read those book-things.');
+  total++;
 } else if (books === 'no' || books === 'n'){
   alert('HAH! you underestimate me, young padawan -- I actually have read two books in one day :D');
 } else {
@@ -71,7 +79,7 @@ if (books === 'yes' || books === 'y') {
 
 console.log('Q 5: ' + questionFive + ' | A: ' + books);
 
-alert('And that, my friends, is IT! I hope you\'ve enjoyed learning a few stupid things about me. Now B E H O L D my mighty CSS! Come back soon.');
+alert('How about answering Question 6, ' + name + '?');
 
 for (var i = 0; i < 4; i++) {
   var questionSix = 'What is my lucky number? Guess between 1 and 10.';
@@ -79,6 +87,7 @@ for (var i = 0; i < 4; i++) {
   if (answer == 7) {
     alert('Woah! You got it!');
     var answerSix = true;
+    total++;
     break;
   } else if (answer < 7) {
     alert('Sorry, that\'s too low :D');
@@ -94,7 +103,9 @@ if (answerSix) {
 } else {
   alert('Sorry, you didn\'t get it. :( Better luck next time.');
 }
-*/
+
+alert(name + '! You\'ve gotten this far! :D Might as well try to answer QUESTION 7!!');
+
 for (var i = 0; i < 6; i++) {
   var questionSeven = 'Guess my favorite food.';
   var answer = prompt(questionSeven);
@@ -104,6 +115,7 @@ for (var i = 0; i < 6; i++) {
   if (food.includes(answer.toLowerCase())) {
     alert('WOW! That\'s right! Good job.');
     var answerSeven = true;
+    total++;
     break;
   } else {
     alert('Sorry, that\'s not one of my favorites :(');
@@ -111,3 +123,11 @@ for (var i = 0; i < 6; i++) {
 }
 
 console.log('Q 7: ' + questionSeven + ' | A: ' + answer);
+
+if (answerSeven) {
+  alert('Congrats on getting the right answer! The possible answers were: ' + food);
+} else {
+  alert('WRONG! :D The answer you SHOULD have given was one of these: ' + food);
+}
+
+alert('You finished! Your final score is ' + total + '/7!!');
